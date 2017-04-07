56e3cb97d02ae822c4a8c2f76ee031b59d792fe1
Miner <Miner>
2017-04-07 14:36:31 -0700

    Changed division to modulus

--- primeFactors.py
+++ primeFactors.py
@@ -6,13 +6,13 @@ then summs results.
 def primeFactor(n):
     total = 0
     while n >3:
-        if n/2 == 0:
+        if n%2 == 0:
             total += 2
             n = n/2
             primeFactor(n)
         else:
             divisor = 3
-            while n/divisor != 0:
+            while n%divisor != 0:
                 divisor += 2
             total += divisor
             n = n/divisor

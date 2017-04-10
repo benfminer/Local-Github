2810ca1b6391641bb1852955337366cededf7ca5
Miner <Miner>
2017-04-10 13:55:50 -0700

    Non recursive

--- primeFactors.py
+++ primeFactors.py
@@ -5,18 +5,16 @@ then summs results.
 
 def primeFactor(n):
     total = 0
-    while n >3:
+    while n !=1:
         if n%2 == 0:
             total += 2
             n = n/2
-            primeFactor(n)
         else:
             divisor = 3
             while n%divisor != 0:
                 divisor += 2
             total += divisor
             n = n/divisor
-            primeFactor(n)
     print(totoal)
 
 n = int(input("Please Enter number: "))

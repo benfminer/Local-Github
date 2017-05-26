5e625a2c9b15fa5a6efefb7100095c53661b1312
Benjamin Miner <benfminer@hotmail.com>
2017-05-26 13:39:45 -0700

    Created Binary Search Tree folder

---
+++ BinarySearchTree/binarySearchTree.py
@@ -0,0 +1,9 @@
+"""
+    Create binary search tree for a list of numbers.
+"""
+
+l = [19,8,33,15,71,12,64,39,41,89,14,56,48,77,25,93,46,70,86,9,78,68]
+
+def binarySearchTree (l):
+    for i in l:
+        print(i)
---
+++ Transform/transform.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+d = ["humic","humid","wane","jade","molt","moll","want","slag","wade","mist","dolt","doll","mate","fade","maze","wart","jake","wary","mitt","wake","gate","mite","wait","faze","malt","hemic","male"]
+
+def transform(beginWord,endWord,d):
+    steps=0
+    d2=d
+    list=[]
+    if beginWord == endWord:
+        return(steps,list)
+    else:
+        for i=
+
+def
+print(sorted(d))

1b0749d26d8c9222533a48d114cab42b7b816689
Benjamin Miner <benfminer@hotmail.com>
2017-05-31 14:54:46 -0700

    Added Best Day function

---
+++ Best Days/bestDays.py
@@ -0,0 +1,8 @@
+"""
+bestDays
+"""
+
+stockValue = [17, 11, 60, 25, 150, 75, 31, 120]
+
+def bestDays(l):
+    
--- BinarySearchTree/binarySearchTree.py
+++ BinarySearchTree/binarySearchTree.py
@@ -2,8 +2,16 @@
     Create binary search tree for a list of numbers.
 """
 
-l = [19,8,33,15,71,12,64,39,41,89,14,56,48,77,25,93,46,70,86,9,78,68]
+import statistics
+
+l = sorted([19,8,33,15,71,12,64,39,41,89,14,56,48,77,25,93,51,46,70,86,9,78,68])
+
+
 
 def binarySearchTree (l):
+    print("The median of the list is " + str(statistics.median(l)))
+    print("The length of the list is " + str(len(l)))
     for i in l:
         print(i)
+
+binarySearchTree(l)
